d3c3b5ec785cee4482b2ed06a91d893ff4ee19ba
Viktor-p13 <Viktor-p13@users.noreply.github.com>
2026-05-27 16:30:58 +0700

    feat: implement Cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -2,4 +2,14 @@ import type { CellValue } from '../types/game'
 
 interface CellProps {
   value: CellValue
+  onClick: () => void
+  disabled: boolean
+}
+
+export function Cell({ value, onClick, disabled }: CellProps) {
+  return (
+    <button type="button" className="cell" onClick={onClick} disabled={disabled}>
+      {value ?? ''}
+    </button>
+  )
 }
