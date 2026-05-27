19bc93b22cc4f31f094d92de2c4f0551feb18bc5
Viktor-p13 <Viktor-p13@users.noreply.github.com>
2026-05-27 16:29:30 +0700

    feat: add getOppositePlayer utility

--- src/utils/board.ts
+++ src/utils/board.ts
@@ -4,3 +4,7 @@ import type { BoardState, Player } from '../types/game'
 export function createEmptyBoard(): BoardState {
   return Array.from({ length: BOARD_SIZE * BOARD_SIZE }, () => null)
 }
+
+export function getOppositePlayer(player: Player): Player {
+  return player === 'X' ? 'O' : 'X'
+}
