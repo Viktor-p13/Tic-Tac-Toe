fbba186ac89e06210c4fa51c096d99b1a8c69177
Viktor-p13 <Viktor-p13@users.noreply.github.com>
2026-05-27 16:30:20 +0700

    feat: scaffold useGame hook

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,13 @@
+import { useCallback, useState } from 'react'
+import type { BoardState, GameState, Player } from '../types/game'
+import { createEmptyBoard, getOppositePlayer } from '../utils/board'
+import { getWinner, isDraw } from '../utils/winner'
+
+function createInitialState(): GameState {
+  return {
+    board: createEmptyBoard(),
+    currentPlayer: 'X',
+    status: 'playing',
+    winner: null,
+  }
+}
